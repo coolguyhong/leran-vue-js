bf284ea90a150f425496055791c4aece7fdf4b27
박준홍/PaaS그룹(기술혁신) <jun64.park@samsungsds-ad.net>
2022-06-27 14:52:07 +0900

    inflearn vue start

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+export default {
+  name: 'App',
+  components: {
+    'app-header': AppHeader
+  },
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hahahahaha'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <form v-on:submit.prevent="submetForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  // 컴포넌트간 데이터가 겹치지 않게 function을 연결해서 새 객체를 return 하는 구문
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submetForm: function() {
+
+      console.log(this.username, this.password)
+      var url = 'https://jsonplaceholder.typicode.com/users'
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
